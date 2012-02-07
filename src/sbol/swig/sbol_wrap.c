5c6bff08914aebf5e9cecf1635103c6c878a9f1f
Jeff Johnson <jefdaj@gmail.com>
2012-02-07 13:59:17 -0800

    moved SWIG files into libSBOLc/src/sbol/swig

--- src/sbol/swig/sbol_wrap.c
+++ src/sbol/swig/sbol_wrap.c
@@ -2700,7 +2700,7 @@ static swig_module_info swig_module = {swig_types, 8, 0, 0, 0, 0};
 #define SWIG_as_voidptrptr(a) ((void)SWIG_as_voidptr(*a),(void**)(a)) 
 
 
-#include "reader.h"
+#include "../reader.h"
 
 
 SWIGINTERN swig_type_info*
