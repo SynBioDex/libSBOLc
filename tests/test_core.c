95ceadf3aab7bac13652a9bc4917beca52f044d5
Jeff Johnson <jefdaj@elitebook.(none)>
2012-02-22 22:49:27 -0800

    added TestInitialNumbers and randomString

--- tests/test_core.c
+++ tests/test_core.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
+#include <time.h>
 #include "CuTest.h"
 #include "core.h"
 
-void TestThatPasses(CuTest* tc) { return; }
+char randomChar()
+{
+	// TODO allow other characters?
+	char* allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int index = rand() % strlen(allowed);
+	return allowed[index];
+}
+
+char* randomString()
+{
+	int length = rand() % 100;
+	char* string = malloc(sizeof(char*) * length);
+	int i;
+	for( i=0; i<length-1; i++ )
+		string[i] = randomChar();
+	string[length] = '\0';
+	return string;
+}
+
+void TestInitialNumbers(CuTest* tc)
+{
+	CuAssertIntEquals(tc, getNumDNAComponents(),		0);
+	CuAssertIntEquals(tc, getNumSequenceAnnotations(),	0);
+	CuAssertIntEquals(tc, getNumCollections(),			0);
+}
+
+void TestGetNumTracking(CuTest* tc);
 
 int main(void)
 {
+	// seed with current time
+	// should only be done once
+	srand( time(NULL) );
+	
 	CuString *output = CuStringNew();
 	CuSuite* suite = CuSuiteNew();
-	SUITE_ADD_TEST(suite, TestThatPasses);
+	SUITE_ADD_TEST(suite, TestInitialNumbers);
 	CuSuiteRun(suite);
 	CuSuiteSummary(suite, output);
 	CuSuiteDetails(suite, output);
