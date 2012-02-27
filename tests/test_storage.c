6114ae220e79ec1065d23e25ca5f808c46276acf
Jeff Johnson <jefdaj@gmail.com>
2012-02-27 09:13:36 -0800

    added testNum... functions

--- tests/test_storage.c
+++ tests/test_storage.c
@@ -23,6 +23,26 @@ void TestSingleComponent(CuTest* tc) {
 	CuAssertIntEquals(tc, 0, isComponentPtr(com));
 }
 
+void TestNumComponents(CuTest* tc) {
+	char* id;
+	DNAComponent* com;
+	int num;
+	for (num=0; num<NUM_FAST_TESTS; num++) {
+		CuAssertIntEquals(tc, num, getNumDNAComponents());
+		id = randomString();
+		com = createComponent(id);
+		registerComponent(com);
+		CuAssertIntEquals(tc, num+1, getNumDNAComponents());
+	}
+	for (; num>0; num--) {
+		CuAssertIntEquals(tc, num, getNumDNAComponents());
+		com = getNthDNAComponent(0);
+		removeComponent(com);
+		CuAssertIntEquals(tc, num-1, getNumDNAComponents());
+	}
+	CuAssertIntEquals(tc, 0, getNumDNAComponents());
+}
+
 void TestSingleAnnotation(CuTest* tc) {
 	CuAssertIntEquals(tc, 0, getNumSequenceAnnotations());
 	SequenceAnnotation* ann = createSequenceAnnotation("one");
@@ -35,6 +55,26 @@ void TestSingleAnnotation(CuTest* tc) {
 	CuAssertIntEquals(tc, 0, getNumSequenceAnnotations());
 	CuAssertIntEquals(tc, 0, isSequenceAnnotationID("one"));
 	CuAssertIntEquals(tc, 0, isAnnotationPtr(ann));
+}
+
+void TestNumAnnotations(CuTest* tc) {
+	char* id;
+	SequenceAnnotation* ann;
+	int num;
+	for (num=0; num<NUM_FAST_TESTS; num++) {
+		CuAssertIntEquals(tc, num, getNumSequenceAnnotations());
+		id = randomString();
+		ann = createSequenceAnnotation(id);
+		registerSequenceAnnotation(ann);
+		CuAssertIntEquals(tc, num+1, getNumSequenceAnnotations());
+	}
+	for (; num>0; num--) {
+		CuAssertIntEquals(tc, num, getNumSequenceAnnotations());
+		ann = getNthSequenceAnnotation(0);
+		removeAnnotation(ann);
+		CuAssertIntEquals(tc, num-1, getNumSequenceAnnotations());
+	}
+	CuAssertIntEquals(tc, 0, getNumSequenceAnnotations());
 }
 
 void TestSingleCollection(CuTest* tc) {
@@ -51,10 +91,33 @@ void TestSingleCollection(CuTest* tc) {
 	CuAssertIntEquals(tc, 0, isCollectionPtr(col));
 }
 
+void TestNumCollections(CuTest* tc) {
+	char* id;
+	Collection* col;
+	int num;
+	for (num=0; num<NUM_FAST_TESTS; num++) {
+		CuAssertIntEquals(tc, num, getNumCollections());
+		id = randomString();
+		col = createCollection(id);
+		registerCollection(col);
+		CuAssertIntEquals(tc, num+1, getNumCollections());
+	}
+	for (; num>0; num--) {
+		CuAssertIntEquals(tc, num, getNumCollections());
+		col = getNthCollection(0);
+		removeCollection(col);
+		CuAssertIntEquals(tc, num-1, getNumCollections());
+	}
+	CuAssertIntEquals(tc, 0, getNumCollections());
+}
+
 CuSuite* StorageGetSuite() {
 	CuSuite* storageTests = CuSuiteNew();
 	SUITE_ADD_TEST(storageTests, TestSingleComponent);
+	SUITE_ADD_TEST(storageTests, TestNumComponents);
 	SUITE_ADD_TEST(storageTests, TestSingleAnnotation);
+	SUITE_ADD_TEST(storageTests, TestNumAnnotations);
 	SUITE_ADD_TEST(storageTests, TestSingleCollection);
+	SUITE_ADD_TEST(storageTests, TestNumCollections);
 	return storageTests;
 }
