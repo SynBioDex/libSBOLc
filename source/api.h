aabac8ff47311f0b3c8fb15b42e762dfa1b86547
Jeff Johnson <jefdaj@gmail.com>
2012-02-27 17:11:08 -0800

    started fixing up the core api

--- source/api.h
+++ source/api.h
@@ -1,45 +1,26 @@
-#include "core.h"
-#include "reader.h"
-#include "writer.h"
-#include "validator.h"
+#ifndef SBOL_CORE_API
+#define SBOL_CORE_API
 
-// TODO make the above depend on this instead
-// (reverse dependencies)
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#	if defined(STATIC_LINKED)
+#		define SBOLAPIEXPORTS
+#	else
+#		if defined(SBOL_EXPORTS)
+#			if defined(USE_STDCALL)
+#				define SBOLAPIEXPORTS __stdcall __declspec(dllexport)
+#			else
+#				define SBOLAPIEXPORTS __declspec(dllexport)
+#			endif
+#		else
+#			define SBOLAPIEXPORTS
+#		endif
+#	endif
+#else
+#	if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
+#		define SBOLAPIEXPORTS __attribute__ ((visibility("default")))
+#	else
+#		define SBOLAPIEXPORTS
+#	endif
+#endif
 
-# ifndef SBOLAPIEXPORTS
-#  if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
-#    if defined(STATIC_LINKED)
-#          define SBOLAPIEXPORTS
-#    else
-#   if defined(SBOL_EXPORTS)
-#              if defined(USE_STDCALL)
-#                   define SBOLAPIEXPORTS __stdcall __declspec(dllexport)
-#              else
-#                   define SBOLAPIEXPORTS __declspec(dllexport)
-#              endif
-#          else
-#              define SBOLAPIEXPORTS
-#          endif
-#     endif
-#  else
-#    if defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
-#      define SBOLAPIEXPORTS __attribute__ ((visibility("default")))
-#    else
-#      define SBOLAPIEXPORTS
-#    endif
-#  endif
-# endif
-
-// TODO these should return the new objects
-SBOLAPIEXPORTS void createDNASequence(const char* id);
-SBOLAPIEXPORTS void createSequenceAnnotation(const char* id);
-SBOLAPIEXPORTS void createDNAComponent(const char* id);
-SBOLAPIEXPORTS void createCollection(const char* id);
-
-SBOLAPIEXPORTS void deleteDNASeqeuence(DNASequence* seq);
-SBOLAPIEXPORTS void deleteSequenceAnnotation(SequenceAnnotation* ann);
-SBOLAPIEXPORTS void deleteDNAComponent(DNAComponent* com);
-SBOLAPIEXPORTS void deleteCollection(Collection* col);
-
-SBOLAPIEXPORTS void cleanup();
-
+#endif
--- source/core.h
+++ source/core.h
@@ -1,5 +1,6 @@
 #ifndef SBOL_CORE
 #define SBOL_CORE
+#include "api.h"
 
 /***********************************************
  * The SBOL core is split into files to ease
@@ -8,6 +9,72 @@
  * #including them as a group from here.
  ***********************************************/
 
-void cleanup();
+// forward declarations
+struct _SequenceAnnotation;
+struct _DNAComponent;
+struct _DNASequence;
+struct _Collection;
+
+// from sequence.h
+SBOLAPIEXPORTS struct _DNASequence* createDNASequence(char* nucleotides);
+SBOLAPIEXPORTS void deleteDNASequence(struct _DNASequence* seq);
+
+// from component.h
+SBOLAPIEXPORTS struct _DNAComponent* createComponent(const char* id);
+SBOLAPIEXPORTS void deleteComponent(struct _DNAComponent* com);
+SBOLAPIEXPORTS void registerComponent(struct _DNAComponent* com);
+SBOLAPIEXPORTS void removeComponent(struct _DNAComponent* com);
+SBOLAPIEXPORTS int isComponentPtr(const void* pointer);
+SBOLAPIEXPORTS int isComponentID(const char* id);
+SBOLAPIEXPORTS int getNumDNAComponents();
+SBOLAPIEXPORTS struct _DNAComponent* getNthDNAComponent(int n);
+SBOLAPIEXPORTS int getNumCollectionsFor(const struct _DNAComponent* com);
+SBOLAPIEXPORTS int getNumSequenceAnnotationsIn(const struct _DNAComponent* com);
+SBOLAPIEXPORTS struct _Collection* getNthCollectionFor(const struct _DNAComponent* com, int n);
+SBOLAPIEXPORTS struct _SequenceAnnotation* getNthSequenceAnnotationIn(const struct _DNAComponent* com, int n);
+SBOLAPIEXPORTS char* getComponentID(const struct _DNAComponent* com);
+SBOLAPIEXPORTS char* getComponentName(const struct _DNAComponent* com);
+SBOLAPIEXPORTS char* getComponentDescription(const struct _DNAComponent* com);
+SBOLAPIEXPORTS void setComponentID(struct _DNAComponent* com, const char* id);
+SBOLAPIEXPORTS void setComponentName(struct _DNAComponent* com, const char* name);
+SBOLAPIEXPORTS void setComponentDescription(struct _DNAComponent* com, const char* desc);
+SBOLAPIEXPORTS void addSequenceAnnotation(struct _DNAComponent* com, struct _SequenceAnnotation* ann);
+SBOLAPIEXPORTS void setSubComponent(struct _SequenceAnnotation* ann, struct _DNAComponent* com);
+
+// from annotation.h
+SBOLAPIEXPORTS void registerSequenceAnnotation(struct _SequenceAnnotation* ann);
+SBOLAPIEXPORTS void removeAnnotation(struct _SequenceAnnotation* ann);
+SBOLAPIEXPORTS int isAnnotationPtr(const void* pointer);
+SBOLAPIEXPORTS int isAnnotationID(const char* id);
+SBOLAPIEXPORTS int getNumSequenceAnnotations();
+SBOLAPIEXPORTS struct _SequenceAnnotation* getNthSequenceAnnotation(int n);
+SBOLAPIEXPORTS struct _SequenceAnnotation* createSequenceAnnotation(const char* id);
+SBOLAPIEXPORTS void setSequenceAnnotationID(struct _SequenceAnnotation* ann, const char* id);
+SBOLAPIEXPORTS void deleteSequenceAnnotation(struct _SequenceAnnotation* ann);
+SBOLAPIEXPORTS void addPrecedesRelationship(struct _SequenceAnnotation* upstream, struct _SequenceAnnotation* downstream);
+SBOLAPIEXPORTS int getNumPrecedes(struct _SequenceAnnotation* ann);
+SBOLAPIEXPORTS struct _SequenceAnnotation* getNthPrecedes(struct _SequenceAnnotation* ann, int n);
+
+// from collection.h
+SBOLAPIEXPORTS struct _Collection* createCollection(const char* id);
+SBOLAPIEXPORTS void deleteCollection(struct _Collection* col);
+SBOLAPIEXPORTS void registerCollection(struct _Collection* col);
+SBOLAPIEXPORTS void removeCollection(struct _Collection* col);
+SBOLAPIEXPORTS int isCollectionPtr(const void* pointer);
+SBOLAPIEXPORTS int isCollectionID(const char* id);
+SBOLAPIEXPORTS int getNumCollections();
+SBOLAPIEXPORTS struct _Collection* getNthCollection(int n);
+SBOLAPIEXPORTS int getNumDNAComponentsIn(struct _Collection* col);
+SBOLAPIEXPORTS struct _DNAComponent* getNthDNAComponentIn(struct _Collection* col, int n);
+SBOLAPIEXPORTS char* getCollectionID(struct _Collection* col);
+SBOLAPIEXPORTS char* getCollectionName(struct _Collection* col);
+SBOLAPIEXPORTS char* getCollectionDescription(struct _Collection* col);
+SBOLAPIEXPORTS void setCollectionID(struct _Collection* col, const char* id); // TODO remove?
+SBOLAPIEXPORTS void setCollectionName(struct _Collection* col, const char* name);
+SBOLAPIEXPORTS void setCollectionDescription(struct _Collection* col, const char* desc);
+SBOLAPIEXPORTS void addComponentToCollection(struct _DNAComponent* com, struct _Collection* col);
+
+// from core.c
+SBOLAPIEXPORTS void cleanup();
 
 #endif
