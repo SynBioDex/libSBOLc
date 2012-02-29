ffcec1f88fa14a2196945854c9f2a85a50f82b2d
Jeff Johnson <jefdaj@gmail.com>
2012-02-28 18:47:27 -0800

    refactored read_ntriples; it's more modular now but still not getting the components, annotations, or subcomponents

--- examples/read_ntriples.c
+++ examples/read_ntriples.c
@@ -2,27 +2,57 @@
 #include "core.h"
 #include "reader.h"
 
-void parse(char* examplepath)
-{
-	int i, j, k;
-	DNAComponent* comp;
-	readSBOL(examplepath);
-	for (i=0; i < getNumDNAComponents(); ++i)
-	{
-		comp = getNthDNAComponent(i);
-		printf("component id = %s\n", comp->id);
-		printf("\tnum. collections = %i\n", getNumCollectionsFor(comp));
-		for (k=0; k < getNumCollectionsFor(comp); ++k)
-			printf("\tcollection = %s\n", getNthCollectionFor(comp,k)->id);
-		printf("\tnum. annotations = %i\n", getNumSequenceAnnotationsIn(comp));
-		if (getNumSequenceAnnotationsIn(comp) > 0)
-			for (j=0; j < getNumSequenceAnnotationsIn(comp); ++j)
-			{
-				SequenceAnnotation* annotation = getNthSequenceAnnotationIn(comp, j);
-				printf("\tsubComponent %s\n", annotation->subComponent->id);
-				for (k=0; k < getNumPrecedes(annotation); ++k)
-					printf("\tprecedes %s\n", getNthPrecedes(annotation, k)->id);
-			}
+// TODO add the print functions to the core for debugging
+
+void printIndent(int tabs) {
+	int i;
+	for (i=0; i<tabs; i++)
+		printf("\t");
+}
+
+void printCollection(Collection* col, int indent) {
+	char* id = col->id;
+	printIndent(indent); printf("collection = %s\n", id);
+}
+
+void printSequenceAnnotation(SequenceAnnotation* ann, int indent) {
+	printIndent(indent); printf("annotation = %s\n", ann->id);
+	printIndent(indent+1); printf("subComponent = %s\n", ann->subComponent->id);
+	int num = getNumPrecedes(ann);
+	int i;
+	char* id;
+	for (i=0; i<num; i++) {
+		id = getNthPrecedes(ann, i)->id;
+		printIndent(indent+1); printf("precedes %s\n", id);
+	}
+}
+
+void printDNAComponent(DNAComponent* com, int indent) {
+	char* id = com->id;
+	int collections = getNumCollectionsFor(com);
+	int annotations = getNumSequenceAnnotationsIn(com);
+	printIndent(indent); printf("%s\n", id);
+	printIndent(indent+1); printf("collections: %i\n", collections);
+	printIndent(indent+1); printf("annotations: %i\n", annotations);
+	int i;
+	for (i=0; i<collections; i++) {
+		Collection* col = getNthCollectionFor(com, i);
+		printCollection(col, indent+2);
+	}
+	for (i=0; i<annotations; i++) {
+		SequenceAnnotation* ann = getNthSequenceAnnotationIn(com, i);
+		printSequenceAnnotation(ann, indent+2);
+	}
+}
+
+void parseAndPrint(char* filename) {
+	readSBOL(filename); // does all the actual parsing
+	DNAComponent* com;
+	printf("components: %i\n", getNumDNAComponents());
+	int i;
+	for (i=0; i < getNumDNAComponents(); i++) {
+		com = getNthDNAComponent(i);
+		printDNAComponent(com, 0);
 		printf("\n");
 	}
 	cleanup();
@@ -35,6 +65,6 @@ int main(int argc, char** argv) {
 		example = "../examples/valid/ntriples.nt";
 	else
 		example = argv[1];
-	parse(example);
+	parseAndPrint(example);
 	return 0;
 }
