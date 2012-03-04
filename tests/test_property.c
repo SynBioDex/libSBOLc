48f96f980e9d2dbdf7695f420c3b5027f7442650
Jeff Johnson <jefdaj@gmail.com>
2012-03-03 20:46:21 -0800

    added more property tests. there's only a couple, but not sure anything else needs to be added for now

--- tests/test_property.c
+++ tests/test_property.c
@@ -4,13 +4,42 @@
 #include "utilities.h"
 #include "property.h"
 
+#define NUM_FAST_TESTS 10000
+
 void TestCreateProperty(CuTest* tc) {
 	Property* pro = createProperty();
 	CuAssertPtrEquals(tc, NULL, getProperty(pro));
+	deleteProperty(pro);
+}
+
+void TestGetSetCompareProperty(CuTest* tc) {
+	Property* pro;
+	char* data;
+	int repeat;
+	// test null
+	pro = createProperty();
+	setProperty(pro, NULL);
+	CuAssertPtrEquals(tc, NULL, getProperty(pro));
+	deleteProperty(pro);
+	// test empty
+	pro = createProperty();
+	setProperty(pro, "");
+	CuAssertStrEquals(tc, "", getProperty(pro));
+	deleteProperty(pro);
+	// test lots of random variations
+	for (repeat=0; repeat<NUM_FAST_TESTS; repeat++) {
+		data = randomString();
+		pro = createProperty();
+		setProperty(pro, data);
+		CuAssertStrEquals(tc, data, getProperty(pro));
+		CuAssertIntEquals(tc, 0, compareProperty(pro, data));
+		deleteProperty(pro);
+	}
 }
 
 CuSuite* PropertyGetSuite() {
 	CuSuite* properyTests = CuSuiteNew();
 	SUITE_ADD_TEST(properyTests, TestCreateProperty);
+	SUITE_ADD_TEST(properyTests, TestGetSetCompareProperty);
 	return properyTests;
 }
