3529d45ddc5852a560e861b74c80527cba23f5ac
Jeff Johnson <jefdaj@gmail.com>
2012-03-11 22:43:42 -0700

    created an OOP-style hierarchy of Properties. they haven't been integrated into the rest of libSBOLc yet, and it won't compile until they are.

--- source/core/property.c
+++ source/core/property.c
@@ -4,42 +4,182 @@
 #include "debug.h"
 #include "property.h"
 
-Property* createProperty() {
-    Property* pro = malloc(sizeof(Property));
-    pro->data = NULL;
+/****************
+ * TextProperty
+ ****************/
+
+TextProperty* createText() {
+    TextProperty* pro = malloc(sizeof(TextProperty));
+    pro->text = NULL;
     return pro;
 }
 
-void deleteProperty(Property* pro) {
+void deleteText(TextProperty* pro) {
     if (pro) {
-        free(pro->data);
-        pro->data = NULL;
+        free(pro->text);
+        pro->text = NULL;
     }
     free(pro);
 }
 
-int compareProperty(const Property* pro, const char* value) {
-    if (!pro || !pro->data || !value)
+int compareText(const TextProperty* pro1, const TextProperty* pro2) {
+    if (!pro1 || !pro2 || !pro1->text || !pro2->text)
         // TODO is this a good error value for strcmp?
         return -1;
-    return strcmp(pro->data, value);
+    return strcmp(pro1->text, pro2->text);
 }
 
-char* getProperty(const Property* pro) {
-    if (!pro || !pro->data)
+char* getText(const TextProperty* pro) {
+    if (!pro || !pro->text)
         return NULL;
-    char* output = malloc(sizeof(char) * strlen(pro->data)+1);
-    strcpy(output, pro->data);
+    char* output = malloc(sizeof(char) * strlen(pro->text)+1);
+    strcpy(output, pro->text);
     return output;
 }
 
-void setProperty(Property* pro, const char* value) {
-    if (!value)
+void setText(TextProperty* pro, const char* text) {
+    if (!pro || !text)
         return;
-    pro->data = realloc(pro->data, sizeof(char) * strlen(value)+1);
-    strcpy(pro->data, value);
+    pro->text = realloc(pro->text, sizeof(char) * strlen(text)+1);
+    strcpy(pro->text, text);
 }
 
-void printProperty(const Property* pro) {
-    printf("%s\n", pro->data);
+void printText(const TextProperty* pro) {
+    if (pro)
+		printf("%s", pro->text);
+}
+
+/***************
+ * URIProperty
+ ***************/
+
+URIProperty* createURI() {
+	return (URIProperty*) createText();
+}
+
+void deleteURI(URIProperty* pro) {
+	deleteText((TextProperty*) pro);
+}
+
+void setURI(URIProperty* pro, const char* uri) {
+	if (pro && !isDuplicateURI(uri))
+		setText((TextProperty*) pro, uri);
+}
+
+char* getURI(const URIProperty* pro) {
+	if (pro)
+		return getText((TextProperty*) pro);
+	else
+		return NULL;
+}
+
+int compareURI(const URIProperty* pro1, const URIProperty* pro2) {
+	return compareText((TextProperty*) pro1, (TextProperty*) pro2);
+}
+
+void printURI(const URIProperty* pro) {
+	printText((TextProperty*) pro);
+}
+
+/***************
+ * IntProperty
+ ***************/
+
+IntProperty* createInt() {
+	IntProperty* pro = malloc(sizeof(IntProperty));
+	pro->number = 0;
+	return pro;
+}
+
+void deleteInt(IntProperty* pro) {
+	if (pro)
+		free(pro); // does this mean anything?
+}
+
+void setInt(IntProperty* pro, int value) {
+	if (pro)
+		pro->number = value;
+}
+
+int getInt(const IntProperty* pro) {
+	if (pro)
+		return pro->number;
+	// TODO "NULL" value?
+}
+
+int compareInt(const IntProperty* pro1, const IntProperty* pro2) {
+	// TODO what's a good NULL value?
+	return pro2->number - pro1->number;
+}
+
+void printInt(const IntProperty* pro) {
+	if (pro)
+		printf("%i", pro->number);
+}
+
+/********************
+ * PositionProperty
+ ********************/
+
+PositionProperty* createPosition() {
+	return (PositionProperty*) createInt();
+}
+
+void deletePosition(PositionProperty* pro) {
+	deleteInt((IntProperty*) pro);
+}
+
+void setPosition(PositionProperty* pro, int pos) {
+	if (pro && pos >= 0)
+		setInt((IntProperty*) pro, pos);
+}
+
+int getPosition(const PositionProperty* pro) {
+	return getInt((IntProperty*) pro);
+}
+
+int comparePosition(const PositionProperty* pro1, const PositionProperty* pro2) {
+	return compareInt((IntProperty*) pro1, (IntProperty*) pro2);
+}
+
+void printPosition(const PositionProperty* pro) {
+	printInt((IntProperty*) pro);
+}
+
+/********************
+ * PolarityProperty
+ ********************/
+
+PolarityProperty* createPolarity() {
+	return (PolarityProperty*) createInt();
+}
+
+void deletePolarity(PolarityProperty* pro) {
+	deleteInt((IntProperty*) pro);
+}
+
+void setPolarity(PolarityProperty* pro, int pol) {
+	if (pro) {
+		if (pol > 0)
+			setInt((IntProperty*) pro, 1);
+		else
+			setInt((IntProperty*) pro, 0);
+	}
+}
+
+int getPolarity(const PolarityProperty* pro) {
+	return getInt((IntProperty*) pro);
+}
+
+int comparePolarity(const PolarityProperty* pro1, const PolarityProperty* pro2) {
+	return compareInt((IntProperty*) pro1, (IntProperty*) pro2);
+}
+
+void printPolarity(const PolarityProperty* pro) {
+	if (pro) {
+		if (getInt((IntProperty*) pro) == 1)
+			printf("+");
+		else
+			printf("-");
+	}
 }
--- source/core/property.h
+++ source/core/property.h
@@ -1,18 +1,79 @@
 #ifndef SBOL_CORE_PROPERTY
 #define SBOL_CORE_PROPERTY
 
-typedef struct _Property {
-    char* data;
-} Property;
+/****************
+ * TextProperty
+ ****************/
 
-// TODO get/setProperty --> get/setPropertyData
+typedef struct _TextProperty {
+	char* text;
+} TextProperty;
 
-Property* createProperty();
-void deleteProperty(Property* pro);
-// TODO compare to other property rather than string?
-int compareProperty(const Property* pro, const char* value);
-char* getProperty(const Property* pro);
-void setProperty(Property* pro, const char* value);
-void printProperty(const Property* pro);
+TextProperty* createText();
+void deleteText(TextProperty* pro);
+void setText(TextProperty* pro, const char* text);
+char* getText(const TextProperty* pro);
+int compareText(const TextProperty* pro1, const TextProperty* pro2);
+void printText(const TextProperty* pro);
+
+/***************
+ * URIProperty
+ ***************/
+
+typedef struct _URIProperty {
+	TextProperty* uri;
+} URIProperty;
+
+URIProperty* createURI();
+void deleteURI(URIProperty* pro);
+void setURI(URIProperty* pro, const char* uri);
+char* getURI(const URIProperty* pro);
+int compareURI(const URIProperty* pro1, const URIProperty* pro2);
+void printURI(const URIProperty* pro);
+
+/***************
+ * IntProperty
+ ***************/
+
+typedef struct _IntProperty {
+	int number;
+} IntProperty;
+
+IntProperty* createInt();
+void deleteInt(IntProperty* pro);
+void setInt(IntProperty* pro, const int value);
+int getInt(const IntProperty* pro);
+int compareInt(const IntProperty* pro1, const IntProperty* pro2);
+void printInt(const IntProperty* pro);
+
+/********************
+ * PositionProperty
+ ********************/
+
+typedef struct _PositionProperty {
+	IntProperty* position;
+} PositionProperty;
+
+PositionProperty* createPosition();
+void deletePosition(PositionProperty* pro);
+void setPosition(PositionProperty* pro, const int value);
+int getPosition(const PositionProperty* pro);
+int comparePosition(const PositionProperty* pro1, const PositionProperty* pro2);
+void printPosition(const PositionProperty* pro);
+
+/********************
+ * PolarityProperty
+ ********************/
+
+typedef struct _PolarityProperty {
+	IntProperty* polarity;
+} PolarityProperty;
+
+PolarityProperty* createPolarity();
+void deletePolarity(PolarityProperty* pro);
+void setPolarity(PolarityProperty* pro, const int value);
+int getPolarity(const PolarityProperty* pro);
+int comparePolarity(const PolarityProperty* pro1, const PolarityProperty* pro2);
+void printPolarity(const PolarityProperty* pro);
 
 #endif
