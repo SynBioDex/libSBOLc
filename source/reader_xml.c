850bb71260f189e8968a3e929c18b05274a6869e
Jeff Johnson <jefdaj@gmail.com>
2012-03-13 13:08:19 -0700

    added the beginnings of an xml-based reader

---
+++ source/reader_xml.c
@@ -0,0 +1,18 @@
+#include "core.h"
+#include "types.h"
+#include "reader.h"
+
+void readDNASequence();
+void readSequenceAnnotation();
+void readDNAComponent();
+void readCollection();
+
+// make a first pass through
+// to create the SBOL objects
+void readSBOLObjects();
+
+// go back and link up the objects 
+// according to rdf:resource nodes
+void readSBOLPointers();
+
+void readSBOLCore(char* filename);
