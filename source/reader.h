2d7bbeb352667ac05209a24e1f07f571b2c3ff23
Jeff Johnson <jefdaj@gmail.com>
2012-03-23 10:50:16 -0700

    added readSBOLCompoundObject

--- source/reader.h
+++ source/reader.h
@@ -1,6 +1,7 @@
 #ifndef SBOL_READER_HEADER
 #define SBOL_READER_HEADER
 #include "api.h"
+#include "object.h"
 
 // TODO remove
 SBOLAPIEXPORTS void readSBOLCore(char* filename);
@@ -11,10 +12,11 @@ xmlChar *getNodeNS(xmlNode *node);
 int nodeNameEquals(xmlNode *node, char *name);
 
 // TODO make static
+SBOLCompoundObject *readSBOLCompoundObject(SBOLCompoundObject *obj, xmlNode *node);
 void readNamespaces(xmlNode *node); // TODO needed?
 void readDNASequence(xmlNode *node);
 void readSequenceAnnotation(xmlNode *node);
-void readDNAComponent(xmlNode *node);
+DNAComponent *readDNAComponent(xmlNode *node);
 void readCollection(xmlNode *node);
 void readReference(xmlNode *node);
 
