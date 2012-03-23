2c4b7e8cd37e2c27049691bbbdae4a6cebce3285
Jeff Johnson <jefdaj@gmail.com>
2012-03-23 09:25:05 -0700

    added some function prototypes

--- source/reader.h
+++ source/reader.h
@@ -2,7 +2,27 @@
 #define SBOL_READER_HEADER
 #include "api.h"
 
+// TODO remove
 SBOLAPIEXPORTS void readSBOLCore(char* filename);
+
+// TODO make static
+xmlChar *getNodeURI(xmlNode *node);
+xmlChar *getNodeNS(xmlNode *node);
+int nodeNameEquals(xmlNode *node, char *name);
+
+// TODO make static
+void readNamespaces(xmlNode *node); // TODO needed?
+void readDNASequence(xmlNode *node);
+void readSequenceAnnotation(xmlNode *node);
+void readDNAComponent(xmlNode *node);
+void readCollection(xmlNode *node);
+void readReference(xmlNode *node);
+
+// TODO make static
+void readSBOLStructs(xmlNode *root);
+void readSBOLPointers(xmlNode *root);
+
+// TODO rename
 SBOLAPIEXPORTS void readSBOLCore_xml(char* filename);
 
 #endif
