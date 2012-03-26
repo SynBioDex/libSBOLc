48ba40df8413ad4f23487ba3fe8bd53284024518
Jeff Johnson <jefdaj@gmail.com>
2012-03-25 18:08:04 -0700

    starting with a blank page

--- source/utilities.h
+++
@@ -1,9 +0,0 @@
-#ifndef SBOL_UTILITIES_HEADER
-#define SBOL_UTILITIES_HEADER
-
-char* getExtension(char* filename);
-int sameString(const char* string1, const char* string2);
-char* intToStr(int input);
-int sameContent(const char* filename1, const char* filename2);
-
-#endif
