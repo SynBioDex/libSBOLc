8dce576ada4f7ff71550ef1b0f39cb7929d760f4
Jeff Johnson <jefdaj@gmail.com>
2012-04-05 19:01:16 -0700

    fixed the object deletion problem

--- wrapper/libsbol.i
+++ wrapper/libsbol.i
@@ -1,44 +1,143 @@
 %module libsbol
 %{
-	#define SWIG_FILE_WITH_INIT
+    #define SWIG_FILE_WITH_INIT
 
-	#include <libxml/xmlmemory.h>
-	#include <libxml/parser.h>
-	#include <libxml/xpath.h>
-	#include <libxml/xmlwriter.h>
-	
-	#include "constants.h"
-	#include "prototypes.h"
-	#include "array.h"
-	#include "utilities.h"
-	#include "collection.h"
-	#include "dnacomponent.h"
-	#include "dnasequence.h"
-	#include "object.h"
-	#include "property.h"
-	#include "reader.h"
-	#include "sequenceannotation.h"
-	#include "validator.h"
-	#include "writer.h"
-	#include "sbol.h"
+    #include <libxml/xmlmemory.h>
+    #include <libxml/parser.h>
+    #include <libxml/xpath.h>
+    #include <libxml/xmlwriter.h>
+
+    #include "constants.h"
+    #include "prototypes.h"
+    #include "array.h"
+    #include "utilities.h"
+    #include "collection.h"
+    #include "dnacomponent.h"
+    #include "dnasequence.h"
+    #include "object.h"
+    #include "property.h"
+    #include "reader.h"
+    #include "sequenceannotation.h"
+    #include "validator.h"
+    #include "writer.h"
+    #include "sbol.h"
 %}
 
-// files in which every function can be included
-%include "constants.h"
-%include "utilities.h"
-%include "object.h"
-%include "dnasequence.h"
-%include "sequenceannotation.h"
-%include "dnacomponent.h"
-%include "collection.h"
-%include "sbol.h"
+// tell SWIG how to free strings
+%typemap(newfree) char* "free($1);";
 
-// functions from libxml
-void xmlFree(xmlChar* str);
-void xmlFreeDoc(xmlDoc* doc); /// @todo remove?
-void xmlCleanupParser(); /// @todo remove?
+// tell SWIG to free the strings returned
+// from these functions immediately
+// (after copying their values into Python)
+%newobject getDNASequenceNucleotides;
+%newobject getDNASequenceURI;
+%newobject getSequenceAnnotationURI;
+%newobject getDNAComponentURI;
+%newobject getDNAComponentDisplayID;
+%newobject getDNAComponentName;
+%newobject getDNAComponentDescription;
+%newobject getCollectionURI;
+%newobject getCollectionDisplayID;
+%newobject getCollectionName;
+%newobject getCollectionDescription;
 
-// functions from files that can't be completely included
-int isValidSBOL(const xmlDocPtr doc);
-void readSBOLCore(char* filename);
+// tell SWIG that these functions delete
+// their first argument, and it doesnt need
+// to be garbage collected
+// (SBOL objects are managed manually anyway,
+//  so this just makes doubly sure to avoid segfaults
+//  from freeing twice)
+%delobject deleteDNASequence;
+%delobject deleteSequenceAnnotation;
+%delobject deleteDNAComponent;
+%delobject deleteCollection;
+
+// functions returing pointers to new objects
+DNASequence* createDNASequence(char* uri);
+DNAComponent* createDNAComponent(const char* uri);
+Collection* createCollection(const char* uri);
+SequenceAnnotation* createSequenceAnnotation(const char* uri);
+
+// functions that delete objects
+void deleteDNASequence(DNASequence* seq);
+void deleteSequenceAnnotation(SequenceAnnotation* ann);
+void deleteDNAComponent(DNAComponent* com);
+void deleteCollection(Collection* col);
+
+// funtions returning strings to be newfree()d
+char* getDNASequenceNucleotides(const DNASequence* seq);
+char* getDNASequenceURI(const DNASequence* seq);
+char* getSequenceAnnotationURI(const SequenceAnnotation* ann);
+char* getDNAComponentURI(const DNAComponent* com);
+char* getDNAComponentDisplayID(const DNAComponent* com);
+char* getDNAComponentName(const DNAComponent* com);
+char* getDNAComponentDescription(const DNAComponent* com);
+char* getCollectionURI(const Collection* col);
+char* getCollectionDisplayID(const Collection* col);
+char* getCollectionName(const Collection* col);
+char* getCollectionDescription(const Collection* col);
+
+// functions returning pointers to existing objects
+DNASequence* getNthDNASequence(int n);
+DNASequence* getDNASequence(const char* uri);
+DNASequence* getDNAComponentSequence(DNAComponent* com);
+SequenceAnnotation* getSequenceAnnotation(const char* uri);
+SequenceAnnotation* getNthSequenceAnnotation(int n);
+SequenceAnnotation* getNthPrecedes(const SequenceAnnotation* ann, int n);
+SequenceAnnotation* getNthSequenceAnnotationFor(const DNAComponent* com, int n);
+DNAComponent* getNthDNAComponent(int n);
+DNAComponent* getDNAComponent(const char* uri);
+DNAComponent* getSequenceAnnotationSubComponent(const SequenceAnnotation* ann);
+DNAComponent* getNthDNAComponentIn(const Collection* col, int n);
+Collection* getCollection(const char* uri);
+Collection* getNthCollection(int n);
+
+// functions returning ints
+int getNumSBOLObjects();
+int isSBOLObjectURI(const char* uri);
+int getNumDNASequences();
+int isDNASequenceURI(const char* uri);
+int isSequenceAnnotation(const void* pointer);
+int isSequenceAnnotationURI(const char* uri);
+int getNumSequenceAnnotations();
+int getSequenceAnnotationStart(const SequenceAnnotation* ann);
+int getSequenceAnnotationEnd(const SequenceAnnotation* ann);
+int getSequenceAnnotationStrand(const SequenceAnnotation* ann);
+int getNumPrecedes(const SequenceAnnotation* ann);
+int precedes(const SequenceAnnotation* upstream, const SequenceAnnotation* downstream);
+int isDNAComponent(const void* pointer);
+int isDNAComponentURI(const char* uri);
+int getNumDNAComponents();
+int getNumSequenceAnnotationsFor(const DNAComponent* com);
+int isCollection(const void* pointer);
+int isCollectionURI(const char* uri);
+int getNumCollections();
+int getNumDNAComponentsIn(const Collection* col);
+int dnaComponentInCollection(const DNAComponent* com, const Collection* col);
 int writeSBOLCore(const char* filename);
+
+// functions returning void
+void setDNASequenceNucleotides(DNASequence* seq, const char* nucleotides);
+void printDNASequence(const DNASequence* seq, int tabs);
+void setSequenceAnnotationStart(SequenceAnnotation* ann, int start);
+void setSequenceAnnotationEnd(SequenceAnnotation* ann, int end);
+void setSequenceAnnotationStrand(SequenceAnnotation* ann, int polarity);
+void addPrecedesRelationship(SequenceAnnotation* upstream, SequenceAnnotation* downstream);
+void printSequenceAnnotation(const SequenceAnnotation* seq, int tabs);
+void setDNAComponentURI(DNAComponent* com, const char* uri);
+void setDNAComponentDisplayID(DNAComponent* com, const char* id);
+void setDNAComponentName(DNAComponent* com, const char* name);
+void setDNAComponentDescription(DNAComponent* com, const char* desc);
+void setDNAComponentSequence(DNAComponent* com, DNASequence* seq);
+void addSequenceAnnotation(DNAComponent* com, SequenceAnnotation* ann);
+void setSequenceAnnotationSubComponent(SequenceAnnotation* ann, DNAComponent* com);
+void printDNAComponent(const DNAComponent* com, int tabs);
+void setCollectionDisplayID(Collection* col, const char* id);
+void setCollectionName(Collection* col, const char* name);
+void setCollectionDescription(Collection* col, const char* desc);
+void addDNAComponentToCollection(DNAComponent* com, Collection* col);
+void printCollection(const Collection* col, int tabs);
+void readSBOLCore(char* filename);
+void printSBOLCore();
+void cleanupSBOLCore();
+
--- wrapper/sbol.py
+++ wrapper/sbol.py
@@ -1,52 +1,54 @@
 import libsbol
 import sys
 from cStringIO import StringIO
-import atexit
+import weakref
 
 __all__ = (
-    'STRAND_FORWARD',
-    'STRAND_BIDIRECTIONAL',
-    'STRAND_REVERSE',
     'DNASequence',
     'SequenceAnnotation',
     'DNAComponent',
     'Collection' )
 
-@atexit.register
-def check_cleanup():
-    print 'sbol objects orphaned in python:', len(ALL_SBOL_OBJECTS)
-    print 'sbol objects orphaned in c:', sbol_swig.getNumSBOLObjects()
+class SBOLObjectRegistry(object):
+    '''
+    Each SBOL object has a ptr attribute that lets
+    you get the SWIG pointer to it. But since pointers are
+    read-only (and unhashable), you need a workaround
+    to go the other way, from pointer --> object.
+    '''
 
-class WrapperError(Exception): 'Problem with the SBOL wrapper'
+    def __init__(self):
+        self.sbol_objects = []
 
-############################################################
-# The sbol objects each have a ptr attribute that lets
-# you get their pointer. But since the pointers are
-# read-only (and unhashable), you need a workaround
-# to go the other way, from pointer --> object.
+    def add(self, obj):
+        '''
+        Create and store a weak reference to obj.
+        It works like a regular referemce, except
+        it doesn't prevent obj from being garbage
+        collected. That's important because otherwise
+        obj.__del__ would never be called.
+        Plus, when obj is garbage collected the 
+        callback will remove the ref.
+        '''
+        callback = self.sbol_objects.remove
+        ref = weakref.ref(obj, callback)
+        self.sbol_objects.append(ref)
 
-ALL_SBOL_OBJECTS = []
+    def find(self, ptr):
+        'Get the SBOL object pointed to by ptr.'
+        for obj in self.sbol_objects:
+            if obj.ptr == ptr:
+                return obj
+        return None
 
-def register_sbol_object(obj):
-    ALL_SBOL_OBJECTS.append(obj)
+ALL_SBOL_OBJECTS = SBOLObjectRegistry()
 
-def retrieve_sbol_object(ptr):
-    for obj in ALL_SBOL_OBJECTS:
-        if obj.ptr == ptr:
-            return obj
-    return None
-
-def remove_sbol_object(obj):
-    if ALL_SBOL_OBJECTS:
-        ALL_SBOL_OBJECTS.remove(obj)
-
-############################################################
-
+# todo attach to print*, not __str__
 def return_stdout(fn):
     '''
     The SBOL print functions use printf() to print directly
     to stdout; this is a workaround that captures that
-    output so Python can use it in __str__ methods.
+    output and returns it for use in Python's __str__ methods.
     '''
     def decorated_fn(*args, **kwargs):
         'Redirect stdout to a str and return it'
@@ -66,99 +68,93 @@ class DNASequence(object):
     
     def __init__(self, uri):
         object.__init__(self)
-        self.ptr = sbol_swig.createDNASequence(uri)
-        register_sbol_object(self)
+        self.ptr = libsbol.createDNASequence(uri)
+        ALL_SBOL_OBJECTS.add(self)
 
     def __del__(self):
-        #remove_sbol_object(self)
-        try:
-            sbol_swig.deleteDNASequence(self.ptr)
-        except TypeError:
-            pass
+        print '%s.__del__' % self.uri
+        libsbol.deleteDNASequence(self.ptr)
 
     @return_stdout
     def __str__(self):
-        sbol_swig.printDNASequence(self.ptr, 0)
+        libsbol.printDNASequence(self.ptr, 0)
 
     @property
     def uri(self):
-        return sbol_swig.getDNASequenceURI(self.ptr)
+        return libsbol.getDNASequenceURI(self.ptr)
 
     @property
     def nucleotides(self):
-        return sbol_swig.getDNASequenceNucleotides(self.ptr)
+        return libsbol.getDNASequenceNucleotides(self.ptr)
 
     @nucleotides.setter
     def nucleotides(self, value):
-        sbol_swig.setDNASequenceNucleotides(self.ptr, value)
+        libsbol.setDNASequenceNucleotides(self.ptr, value)
 
 class SequenceAnnotation(object):
     'Implements the SBOL Core SequenceAnnotation object'
     
     def __init__(self, uri):
         object.__init__(self)
-        self.ptr = sbol_swig.createSequenceAnnotation(uri)
-        register_sbol_object(self)
+        self.ptr = libsbol.createSequenceAnnotation(uri)
+        ALL_SBOL_OBJECTS.add(self)
 
     def __del__(self):
-        #remove_sbol_object(self)
-        try:
-            sbol_swig.deleteSequenceAnnotation(self.ptr)
-        except TypeError:
-            pass
+        print '%s.__del__' % self.uri
+        libsbol.deleteSequenceAnnotation(self.ptr)
 
     @return_stdout
     def __str__(self):
-        sbol_swig.printSequenceAnnotation(self.ptr, 0)
+        libsbol.printSequenceAnnotation(self.ptr, 0)
 
     @property
     def uri(self):
-        return sbol_swig.getSequenceAnnotationURI(self.ptr)
+        return libsbol.getSequenceAnnotationURI(self.ptr)
 
     @property
     def start(self):
-        return sbol_swig.getSequenceAnnotationNucleotides(self.ptr)
+        return libsbol.getSequenceAnnotationNucleotides(self.ptr)
 
     @start.setter
     def start(self, index):
-        sbol_swig.setSequenceAnnotationStart(self.ptr, index)
+        libsbol.setSequenceAnnotationStart(self.ptr, index)
         
     @property
     def end(self):
-        return sbol_swig.getSequenceAnnotationEnd(self.ptr)
+        return libsbol.getSequenceAnnotationEnd(self.ptr)
 
     @end.setter
     def end(self, index):
-        sbol_swig.setSequenceAnnotationEnd(self.ptr, index)
+        libsbol.setSequenceAnnotationEnd(self.ptr, index)
 
     @property
     def strand(self):
-        return sbol_swig.getSequenceAnnotationStrand(self.ptr)
+        return libsbol.getSequenceAnnotationStrand(self.ptr)
 
     # doesn't appear to work
     @strand.setter
     def strand(self, polarity):
-        sbol_swig.setSequenceAnnotationStrand(self.ptr, polarity)
+        libsbol.setSequenceAnnotationStrand(self.ptr, polarity)
 
     @property
     def subcomponent(self):
-        ptr = sbol_swig.getSequenceAnnotationSubComponent(self.ptr)
+        ptr = libsbol.getSequenceAnnotationSubComponent(self.ptr)
         return retrieve_sbol_object(ptr)
 
     @subcomponent.setter
     def subcomponent(self, com):
-        sbol_swig.setSequenceAnnotationSubComponent(self.ptr, com)
+        libsbol.setSequenceAnnotationSubComponent(self.ptr, com)
 
     # can this be done with a decorator?
     def add_precedes(self, downstream):
-        sbol_swig.addPrecedesRelationship(self.ptr, downstream.ptr)
+        libsbol.addPrecedesRelationship(self.ptr, downstream.ptr)
 
     @property
     def precedes(self):
         precedes = []
-        num = sbol_swig.getNumPrecedes(self.ptr)
+        num = libsbol.getNumPrecedes(self.ptr)
         for n in range(num):
-            ptr = sbol_swig.getNthPrecedes(self.ptr, n)
+            ptr = libsbol.getNthPrecedes(self.ptr, n)
             precedes.append( retrieve_sbol_object(ptr) )
         return precedes
 
@@ -167,69 +163,66 @@ class DNAComponent(object):
     
     def __init__(self, uri):
         object.__init__(self)
-        self.ptr = sbol_swig.createDNAComponent(uri)
-        register_sbol_object(self)
+        self.ptr = libsbol.createDNAComponent(uri)
+        ALL_SBOL_OBJECTS.add(self)
 
     def __del__(self):
-        #remove_sbol_object(self)
-        try:
-            sbol_swig.deleteDNAComponent(self.ptr)
-        except TypeError:
-            pass
+        print '%s.__del__' % self.uri
+        libsbol.deleteDNAComponent(self.ptr)
             
     @return_stdout
     def __str__(self):
-        sbol_swig.printDNAComponent(self.ptr, 0)
+        libsbol.printDNAComponent(self.ptr, 0)
 
     @property
     def uri(self):
-        return sbol_swig.getDNAComponentURI(self.ptr)
+        return libsbol.getDNAComponentURI(self.ptr)
 
     @property
     def display_id(self):
-        return sbol_swig.getDNAComponentDisplayID(self.ptr)
+        return libsbol.getDNAComponentDisplayID(self.ptr)
 
     @property
     def name(self):
-        return sbol_swig.getDNAComponentName(self.ptr)
+        return libsbol.getDNAComponentName(self.ptr)
 
     @property
     def description(self):
-        return sbol_swig.getDNAComponentDescription(self.ptr)
+        return libsbol.getDNAComponentDescription(self.ptr)
 
     @property
     def sequence(self):
-        ptr = sbol_swig.getDNAComponentSequence(self.ptr)
+        ptr = libsbol.getDNAComponentSequence(self.ptr)
         return retrieve_sbol_object(ptr)
 
     @property
     def annotations(self):
         annotations = []
-        num = sbol_swig.getNumSequenceAnnotationsFor(self.ptr)
+        num = libsbol.getNumSequenceAnnotationsFor(self.ptr)
         for n in range(num):
-            ptr = sbol_swig.getNthSequenceAnnotationFor(self.ptr, n)
+            ptr = libsbol.getNthSequenceAnnotationFor(self.ptr, n)
             annotations.append( retrieve_sbol_object(ptr) )
         return annotations
 
     @display_id.setter
     def display_id(self, id):
-        sbol_swig.setDNAComponentDisplayID(self.ptr, id)
+        libsbol.setDNAComponentDisplayID(self.ptr, id)
 
     @name.setter
     def name(self, name):
-        sbol_swig.setDNAComponentName(self.ptr, name)
+        libsbol.setDNAComponentName(self.ptr, name)
 
     @description.setter
     def description(self, descr):
-        sbol_swig.setDNAComponentDescription(self.ptr, descr)
+        libsbol.setDNAComponentDescription(self.ptr, descr)
 
     @sequence.setter
     def sequence(self, seq):
-        sbol_swig.setDNAComponentSequence(self.ptr, seq.ptr)
+        libsbol.setDNAComponentSequence(self.ptr, seq.ptr)
 
     # can this be done with a property?
     def add_annotation(self, ann):
-        sbol_swig.addSequenceAnnotation(self.ptr, ann.ptr)
+        libsbol.addSequenceAnnotation(self.ptr, ann.ptr)
 
     # todo remove_annotation?
 
@@ -238,59 +231,55 @@ class Collection(object):
 
     def __init__(self, uri):
         object.__init__(self)
-        self.ptr = sbol_swig.createCollection(uri)
-        register_sbol_object(self)
+        self.ptr = libsbol.createCollection(uri)
+        ALL_SBOL_OBJECTS.add(self)
 
-    # why does this throw an exception during shutdown?
     def __del__(self):
-        #remove_sbol_object(self)
-        try:
-            sbol_swig.deleteCollection(self.ptr)
-        except TypeError: # TypeError("'NoneType' object is not callable",)
-            pass
+        print '%s.__del__' % self.uri
+        libsbol.deleteCollection(self.ptr)
 
     @return_stdout
     def __str__(self):
-        sbol_swig.printCollection(self.ptr, 0)
+        libsbol.printCollection(self.ptr, 0)
 
     @property
     def uri(self):
-        return sbol_swig.getCollectionURI(self.ptr)
+        return libsbol.getCollectionURI(self.ptr)
 
     @property
     def display_id(self):
-        return sbol_swig.getCollectionDisplayID(self.ptr)
+        return libsbol.getCollectionDisplayID(self.ptr)
 
     @property
     def name(self):
-        return sbol_swig.getCollectionName(self.ptr)
+        return libsbol.getCollectionName(self.ptr)
 
     @property
     def description(self):
-        return sbol_swig.getCollectionDescription(self.ptr)
+        return libsbol.getCollectionDescription(self.ptr)
 
     @property
     def components(self):
         components = []
-        num = sbol_swig.getNumDNAComponentsIn(self.ptr)
+        num = libsbol.getNumDNAComponentsIn(self.ptr)
         for n in range(num):
-            ptr = sbol_swig.getNthDNAComponentIn(self.ptr, n)
+            ptr = libsbol.getNthDNAComponentIn(self.ptr, n)
             components.append( retrieve_sbol_object(ptr) )
         return components
 
     @display_id.setter
     def display_id(self, id):
-        sbol_swig.setCollectionDisplayID(self.ptr, id)
+        libsbol.setCollectionDisplayID(self.ptr, id)
 
     @name.setter
     def name(self, name):
-        sbol_swig.setCollectionName(self.ptr, name)
+        libsbol.setCollectionName(self.ptr, name)
 
     @description.setter
     def description(self, descr):
-        sbol_swig.setCollectionDescription(self.ptr, descr)
+        libsbol.setCollectionDescription(self.ptr, descr)
 
     # can this be done with a decorator?
     def add_component(self, com):
-        sbol_swig.addDNAComponentToCollection(com.ptr, self.ptr)
+        libsbol.addDNAComponentToCollection(com.ptr, self.ptr)
 
